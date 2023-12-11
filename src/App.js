590e4141ac54b59fe93f7a8bd767cdb6998a549d
r123-dev <rohanmourya879@gmail.com>
2023-12-11 22:39:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const CountdownTimer = () => {
+  const [timeInput, setTimeInput] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(0);
+  const [timerRunning, setTimerRunning] = useState(false);
 
-function App() {
+  useEffect(() => {
+    let timer;
+
+    if (timerRunning && timeRemaining > 0) {
+      timer = setInterval(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timerRunning && timeRemaining === 0) {
+      alert("Time's up!");
+      setTimerRunning(false);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timerRunning, timeRemaining]);
+
+  const startTimer = () => {
+    const totalTimeInSeconds = timeInput * 60;
+
+    setTimeRemaining(totalTimeInSeconds);
+    setTimerRunning(true);
+  };
+
+  const pauseTimer = () => {
+    setTimerRunning(false);
+  };
+
+  const updateTimerDisplay = () => {
+    const hours = Math.floor(timeRemaining / 3600);
+    const minutes = Math.floor((timeRemaining % 3600) / 60);
+    const seconds = timeRemaining % 60;
+
+    const formattedTime = `${padZero(hours)}:${padZero(minutes)}:${padZero(
+      seconds
+    )}`;
+    return formattedTime;
+  };
+  const checktime = () => {
+    setTimeRemaining(0);
+    setTimerRunning(false);
+  };
+  const padZero = (value) => {
+    return value < 10 ? `0${value}` : value;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="maincomponent">
+        <div className="second">
+          <div className="enter">
+            <label htmlFor="timeInput">Enter Minutes</label>
+          </div>
+          <div className="inp">
+            <input
+              type="text"
+              id="timeInput"
+              value={timeInput}
+              onChange={(e) => {
+                setTimeInput(e.target.value);
+                setTimerRunning(false);
+                setTimeRemaining(0);
+              }}
+            />
+          </div>
+        </div>
 
-export default App;
+        <div id="timer">{updateTimerDisplay()}</div>
+        <div id="controls">
+          <button onClick={startTimer}>Start</button>
+          <button onClick={pauseTimer}>Pause</button>
+          <button onClick={checktime}>Reset</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CountdownTimer;
